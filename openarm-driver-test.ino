7be7f02755d42e7e7b1e2b62295ac444a76c9e99
Colton Fox <coltonfox2004@gmail.com>
2025-04-03 22:06:47 -0400

    initial commit

---
+++ openarm-driver-test.ino
@@ -0,0 +1,31 @@
+#include "bldc_driver.h"
+
+// Hin: Highside PWM, Sin: Shutdown pin, Cur: current sense pin (input)
+#define HIN_A 11   // High-side PWM
+#define SIN_A 7    // Shutdown pin
+#define CUR_A A1    // Current sense
+
+#define HIN_B 10
+#define SIN_B 6
+#define CUR_B A2
+
+#define HIN_C 9
+#define SIN_C 5
+#define CUR_C A3
+
+BLDCDriver driver(HIN_A, SIN_A, CUR_A,
+                  HIN_B, SIN_B, CUR_B,
+                  HIN_C, SIN_C, CUR_C);
+
+void setup() {
+  driver.begin();
+  driver.start();
+
+  driver.setSpeed(127);
+  // pinMode(11, OUTPUT);
+}
+
+void loop() {
+  driver.commutate();
+  // analogWrite(11, 127);
+}
